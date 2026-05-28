7365429e7acaf4d5c5c6c4756fec02a9a7e279c0
Jokler <github@jokler.xyz>
2026-05-28 13:25:21 +0200

    Log metadata errors on cli scan

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,10 @@
 use std::{fs::File, io::Read, path::Path};
 
+use anyhow::Error;
 use axum::body::Bytes;
 use diesel::{ExpressionMethods, OptionalExtension};
 use diesel_async::RunQueryDsl;
-use tracing::{debug, info};
+use tracing::{debug, error, info};
 
 use crate::{
     SqlitePool,
@@ -30,45 +31,55 @@ pub async fn scan_music(music_root: &Path, pool: &SqlitePool) {
             let mut data = Vec::new();
             file.read_to_end(&mut data).unwrap();
             let data = Bytes::from_owner(data);
-            if let Ok(metadata) = metadata(&data).await {
-                debug!(path = ?file_path, "Found metadata");
-                let file_id: Option<i64> = diesel::insert_into(audio_files::table)
-                    .values((
-                        audio_files::file_name.eq(&file_name.to_str().unwrap()),
-                        audio_files::file_path
-                            .eq(&file_path.to_str().expect("FIXME: paths can be non UTF-8")),
-                        audio_files::duration.eq(DbDuration::from(metadata.duration)),
-                        audio_files::created_at.eq(unix_timestamp()),
-                        audio_files::created_by.eq("cli-scan"),
-                    ))
-                    .on_conflict(audio_files::file_path)
-                    .do_nothing()
-                    .returning(audio_files::id)
-                    .get_result(&mut conn)
-                    .await
-                    .optional()
-                    .unwrap();
+            match metadata(&data).await {
+                Ok(metadata) => {
+                    debug!(path = ?file_path, "Found metadata");
+                    let file_id: Option<i64> = diesel::insert_into(audio_files::table)
+                        .values((
+                            audio_files::file_name.eq(&file_name.to_str().unwrap()),
+                            audio_files::file_path
+                                .eq(&file_path.to_str().expect("FIXME: paths can be non UTF-8")),
+                            audio_files::duration.eq(DbDuration::from(metadata.duration)),
+                            audio_files::created_at.eq(unix_timestamp()),
+                            audio_files::created_by.eq("cli-scan"),
+                        ))
+                        .on_conflict(audio_files::file_path)
+                        .do_nothing()
+                        .returning(audio_files::id)
+                        .get_result(&mut conn)
+                        .await
+                        .optional()
+                        .unwrap();
 
-                if let Some(title) = metadata.title
-                    && let Some(artist) = metadata.artist
-                    && let Some(file_id) = file_id
-                {
-                    debug!("Updating metadata");
-                    let song = update_song_metadata(
-                        &mut conn,
-                        "cli-scan",
-                        file_id,
-                        PutSongMetadata {
-                            track: metadata.track,
-                            title,
-                            artist,
-                            album: metadata.album,
-                            cover_path: metadata.cover_path,
-                        },
-                    )
-                    .await
-                    .unwrap();
-                    info!("Added {} - {}", song.title, song.artist);
+                    if let Some(title) = metadata.title
+                        && let Some(artist) = metadata.artist
+                        && let Some(file_id) = file_id
+                    {
+                        debug!("Updating metadata");
+                        let song = update_song_metadata(
+                            &mut conn,
+                            "cli-scan",
+                            file_id,
+                            PutSongMetadata {
+                                track: metadata.track,
+                                title,
+                                artist,
+                                album: metadata.album,
+                                cover_path: metadata.cover_path,
+                            },
+                        )
+                        .await
+                        .unwrap();
+                        info!("Added {} - {}", song.title, song.artist);
+                    }
+                }
+                Err(e) => {
+                    let e = Error::new(e);
+                    let err = e
+                        .chain()
+                        .skip(1)
+                        .fold(e.to_string(), |acc, cause| format!("{}: {}", acc, cause));
+                    error!(path=?file_path, error = %err, "Failed to find metadata");
                 }
             }
         }
